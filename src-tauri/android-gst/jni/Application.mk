603d2dd26f21545c02365bc2d862e6843df71a5a
Nishal <nishal@example.com>
2026-08-01 15:56:41 +0530

    fixeeee 2

--- src-tauri/android-gst/jni/Application.mk
+++ src-tauri/android-gst/jni/Application.mk
@@ -1,2 +1,5 @@
-APP_ABI := arm64-v8a armeabi-v7a x86 x86_64
+APP_ABI := arm64-v8a
 APP_PLATFORM := android-24
+# Help ndk-build find GStreamer headers when building gstreamer_android.c
+APP_CFLAGS := -I$(GSTREAMER_ROOT_ANDROID)/arm64/include -I$(GSTREAMER_ROOT_ANDROID)/arm64/include/gstreamer-1.0 -I$(GSTREAMER_ROOT_ANDROID)/arm64/include/glib-2.0 -I$(GSTREAMER_ROOT_ANDROID)/arm64/lib/glib-2.0/include
+APP_CPPFLAGS := $(APP_CFLAGS)
